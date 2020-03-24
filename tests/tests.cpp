cc7daa565c6d083dde8ff87d1a991204797098d5
nite <darkdukey@gmail.com>
2020-03-23 21:08:12 -0700

    Init commit

---
+++ tests/tests.cpp
@@ -0,0 +1,38 @@
+#include "FileUtil.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+using namespace NT::FileUtil;
+
+// Setup test
+class UtilTest : public ::testing::Test {
+   protected:
+    UtilTest() {}
+    ~UtilTest() override {}
+};
+
+TEST_F(UtilTest, File_Name) {
+    auto filename = getFileName("hello\\test\\test.txt");
+    EXPECT_EQ(filename, "test.txt");
+
+    filename = getFileName("hello/test/test2.txt");
+    EXPECT_EQ(filename, "test2.txt");
+
+    filename = getFileName("");
+    EXPECT_EQ(filename, "");
+
+    auto file_ext = getExtension("hello\\test\\test.txt");
+    EXPECT_EQ(file_ext, "txt");
+}
+
+TEST_F(UtilTest, File_Read) {
+    string file_path = "../tests/data/test1.txt";
+    auto content = readFile(file_path);
+    EXPECT_EQ(content, "test is test1.txt");
+
+    vector<unsigned char> data;
+    bool result = loadFile(file_path, data);
+
+    EXPECT_EQ(data.size(), 17);
+    EXPECT_EQ(data[0], 't');
+}
